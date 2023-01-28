ad7dc07863b29c0a9993ee6be71c4228b00085c0
trishapakhi <vijaya.mistri@gmail.com>
2023-01-28 16:02:39 +0530

    setup tailwind

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
+import Layout from 'components/Layout/Layout'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>Home</Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
