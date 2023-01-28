6f9d9ae65fc9f406942eefd6a4abe0eb001334ad
trishapakhi <vijaya.mistri@gmail.com>
2023-01-28 14:35:53 +0530

    create home

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='grid grid-cols-4 gap-4 w-full'>
+            <div>jfh</div>
+            <div>jfjhiuj</div>
+        </div>
+    )
+}
+
+export default Home
