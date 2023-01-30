32979c762faf1ae405dd7eb19191fadd8d2918e1
rohit155140 <rohitkumar63361@gmail.com>
2023-01-30 17:28:05 +0530

    working on tab and model animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Image from 'next/image'
 import { FaAlignLeft } from 'react-icons/fa'
 import Link from 'next/link';
 import { AiOutlineClose } from 'react-icons/ai'
-import Zoom from 'react-reveal/Zoom';
+import Fade from 'react-reveal/Fade';
 
 const Test = () => {
 
@@ -25,9 +25,9 @@ const Test = () => {
 
 
             <Head> LearnDash </Head>
-            <div className={`grid grid-cols-1 md:grid-cols-2   md:h-screen bg-[url('/images/bg-image.png')] bg-cover bg-center bg-no-repeat`}>
+            <div className={`grid grid-cols-1 md:grid-cols-12   md:h-screen bg-[url('/images/bg-image.png')] bg-cover bg-center bg-no-repeat`}>
 
-                <div className='flex flex-col justify-between p-10'>
+                <div className='flex flex-col justify-between p-10 col-span-6'>
                     <Image src='/images/logo.png' height='70' width='250' alt='logo' />
                     <div className='w-full'>
                         <p className='text-5xl'>massage ce.</p>
@@ -35,9 +35,9 @@ const Test = () => {
                     </div>
                 </div>
 
-                {!open ? <div className='grid grid-cols-1 relative md:divide-x-[1px] md:divide-bordergray md:grid-cols-7
-                 '>
-                    <div className='col-span-7 md:h-screen md:col-span-3'>
+
+                {!open ? <div className='col-span-5 grid grid-cols-12'>
+                    <div className='col-span-6 md:h-screen md:col-span-6'>
                         <div className='grid md:h-full grid-cols-1 divide-y-[1px] divide-bordergray md:border-0 border-y-[1px] border-bordergray'>
                             <div className='flex flex-col justify-center border-l border-l-bordergray  p-2 zoom-effect-container'>
                                 <div className='flex space-x-4 md:block md:space-x-0 image-card'>
@@ -64,7 +64,7 @@ const Test = () => {
                         </div>
                     </div>
 
-                    <div className='col-span-7 md:h-screen md:col-span-3'>
+                    <div className='col-span-12 md:h-screen md:col-span-6 border-l border-bordergray'>
                         <div className='grid h-full grid-cols-1 divide-y-[1px] divide-bordergray'>
                             <div className='flex flex-col justify-center p-2 zoom-effect-container'>
                                 <div className='flex space-x-4 md:block md:space-x-0 image-card'>
@@ -98,112 +98,89 @@ const Test = () => {
 
                         </div>
                     </div>
+                </div> : <div className=' col-span-5 relative '>
+                    <Fade up>
+                        <div className={`  absolute  top-1 left-1 transition-all  `}>
 
-                    <div className='col-span-7 p-4 md:col-span-1 relative h-screen w-full'>
-                        <div className='flex items-center justify-center space-x-2 absolute top-1'>
-                            <p className='font-semibold'>menu</p>
-                            <Image src="/images/menuIcon.png" width={25} height={25} alt="menu" />
-                        </div>
-                        <div className='flex items-center justify-center space-x-2 absolute bottom-5 '>
-                            <p className='font-semibold'>on</p>
-                            <Image src="/images/sound.png" width={25} height={25} alt="sound" />
-                        </div>
-                    </div>
-
-                </div> : ""}
+                            <div className="   mt-48 px-40">
 
 
-                {open ?
-                    <div className=" relative grid grid-cols-12  z-40">
-
-                        <div className='md:col-span-5 col-span-12'>
-
-                        </div>
-                        <div className=' md:col-span-5 col-span-12'>
-                            <Zoom>
-                                <div className={`  absolute inset-0  `}>
-
-                                    <div className="   mt-48 px-48">
-
-
-                                        <div className="w-full  m-auto bg-transparent  border border-bordergray p-4 lg:max-w-sm">
-                                            <div className='flex justify-between items-center'>
-                                                <h2 className='font-semibold py-2'>Welcome Back !</h2>
-                                                <button onClick={() => setOpen(false)}><AiOutlineClose /></button>
-                                            </div>
-
-                                            <form className="">
-                                                <div className="mb-2">
-                                                    <label
-                                                        for="email"
-                                                        className="block text-sm font-semibold text-gray-800"
-                                                    >
-                                                        Email
-                                                    </label>
-                                                    <input
-                                                        type="email"
-                                                        className="block w-full px-4 py-2 mt-2  bg-white border border-bordergray   focus:outline-none "
-                                                        placeholder='enter your mail'
-                                                    />
-                                                </div>
-                                                <div className="mb-2">
-                                                    <label
-                                                        for="password"
-                                                        className="block text-sm font-semibold text-gray-800"
-
-                                                    >
-                                                        Password
-                                                    </label>
-                                                    <input
-                                                        type="password"
-                                                        className="block w-full px-4 py-2 mt-2  bg-white border  border-bordergray    focus:outline-none "
-                                                        placeholder='**********'
-                                                    />
-                                                </div>
-                                                <div className='flex justify-between items-center'>
-                                                    <div className='flex space-x-1 items-center'>
-
-                                                        <div class="form-check">
-                                                            <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault" />
-
-                                                        </div>
-
-                                                        <p>remember me</p>
-                                                    </div>
-                                                    <Link href="#">
-                                                        <p className='font-bold'>forget Password</p>
-                                                    </Link>
-
-                                                </div>
-                                                <div className="mt-6 mx-6">
-                                                    <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-black rounded-3xl focus:outline-none">
-                                                        Login
-                                                    </button>
-                                                </div>
-                                            </form>
-                                        </div>
-
+                                <div className="w-full  m-auto bg-transparent  border border-bordergray p-4 lg:max-w-sm">
+                                    <div className='flex justify-between items-center'>
+                                        <h2 className='font-semibold py-2'>Welcome Back !</h2>
+                                        <button onClick={() => setOpen(false)}><AiOutlineClose /></button>
                                     </div>
 
+                                    <form className="">
+                                        <div className="mb-2">
+                                            <label
+                                                for="email"
+                                                className="block text-sm font-semibold text-gray-800"
+                                            >
+                                                Email
+                                            </label>
+                                            <input
+                                                type="email"
+                                                className="block w-full px-4 py-2 mt-2  bg-white border border-bordergray   focus:outline-none "
+                                                placeholder='enter your mail'
+                                            />
+                                        </div>
+                                        <div className="mb-2">
+                                            <label
+                                                for="password"
+                                                className="block text-sm font-semibold text-gray-800"
 
+                                            >
+                                                Password
+                                            </label>
+                                            <input
+                                                type="password"
+                                                className="block w-full px-4 py-2 mt-2  bg-white border  border-bordergray    focus:outline-none "
+                                                placeholder='**********'
+                                            />
+                                        </div>
+                                        <div className='flex justify-between items-center'>
+                                            <div className='flex space-x-1 items-center'>
+
+                                                <div class="form-check">
+                                                    <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault" />
+
+                                                </div>
+
+                                                <p>remember me</p>
+                                            </div>
+                                            <Link href="#">
+                                                <p className='font-bold'>forget Password</p>
+                                            </Link>
+
+                                        </div>
+                                        <div className="mt-6 mx-6">
+                                            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-black rounded-3xl focus:outline-none">
+                                                Login
+                                            </button>
+                                        </div>
+                                    </form>
                                 </div>
-                            </Zoom>
-                        </div>
 
-                        <div className='col-span-12 md:col-span-2 mt-5'>
-                            <div className='flex  justify-center  space-x-2'>
-                                <p className='font-semibold '>menu</p>
-                                <FaAlignLeft />
                             </div>
+
+
                         </div>
+                    </Fade>
+                </div>}
 
 
 
-
-
-
+                <div className='col-span-12 p-4 md:col-span-1 relative h-screen w-full border-l border-bodergray '>
+                    <div className='flex items-center justify-center space-x-2 absolute top-1'>
+                        <p className='font-semibold'>menu</p>
+                        <Image src="/images/menuIcon.png" width={25} height={25} alt="menu" />
                     </div>
-                    : ""}
+                    <div className='flex items-center justify-center space-x-2 absolute bottom-5 '>
+                        <p className='font-semibold'>on</p>
+                        <Image src="/images/sound.png" width={25} height={25} alt="sound" />
+                    </div>
+                </div>
 
             </div>
 
