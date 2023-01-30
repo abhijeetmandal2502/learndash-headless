1c9f499b96ba56c69346692a488fb73ef542d40f
trishapakhi <vijaya.mistri@gmail.com>
2023-01-30 10:09:45 +0530

    update  css

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,12 +19,13 @@ export default function Home() {
         <div className='grid grid-cols-1 md:divide-x-[1px] md:divide-bordergray md:grid-cols-7'>
           <div className='col-span-7 md:h-screen md:col-span-3'>
             <div className='grid md:h-full grid-cols-1 divide-y-[1px] divide-bordergray md:border-0 border-y-[1px] border-bordergray'>
+
               <div className='flex flex-col justify-center p-2 zoom-effect-container'>
                 <div className='flex space-x-4 md:block md:space-x-0 image-card'>
                   <Image alt='start' src='/images/start.png' height='155' width='180' className='md:mx-auto ' />
                   <div className='w-full'>
                     <h3 className='mt-4 font-semibold md:text-center'>Start</h3>
-                    <p className='mt-2 md:text-center lineUp'>have a question? get in touch!</p>
+                    <p className='  md:text-center lineUp'>have a question? get in touch!</p>
                   </div>
                 </div>
               </div>
@@ -34,7 +35,7 @@ export default function Home() {
                   <Image alt='start' src='/images/Im-Back.png' height='65' width='65' className='md:mx-auto ' />
                   <div className='w-full'>
                     <h3 className='mt-4 font-semibold md:text-center'>I Am Back</h3>
-                    <p className='mt-2 md:text-center lineUp'>have a question? get in touch!</p>
+                    <p className='  md:text-center lineUp'>have a question? get in touch!</p>
                   </div>
                 </div>
               </div>
@@ -48,7 +49,7 @@ export default function Home() {
                   <Image alt='start' src='/images/Concierge.png' height='65' width='65' className='md:mx-auto ' />
                   <div className='w-full'>
                     <h3 className='mt-4 font-semibold md:text-center'>Concierge</h3>
-                    <p className='mt-2 md:text-center lineUp'>have a question? get in touch!</p>
+                    <p className='  md:text-center lineUp'>have a question? get in touch!</p>
                   </div>
                 </div>
               </div>
@@ -58,7 +59,7 @@ export default function Home() {
                   <Image alt='start' src='/images/NCBTMB-Approved.png' height='120' width='120' className='md:mx-auto ' />
                   <div className='w-full'>
                     <h3 className='mt-4 font-semibold md:text-center'>NCBTMB Approved</h3>
-                    <p className='mt-2 md:text-center lineUp'>have a question? get in touch!</p>
+                    <p className=' md:text-center lineUp'>have a question? get in touch!</p>
                   </div>
                 </div>
               </div>
@@ -68,7 +69,7 @@ export default function Home() {
                   <Image alt='start' src='/images/Teacher-Lounge.png' height='70' width='51' className='md:mx-auto ' />
                   <div className='w-full'>
                     <h3 className='font-semibold md:text-center md:mt-4'>Teachers Lounge</h3>
-                    <p className='mt-2 md:text-center lineUp'>have a question? get in touch!</p>
+                    <p className=' md:text-center lineUp'>have a question? get in touch!</p>
                   </div>
                 </div>
               </div>
