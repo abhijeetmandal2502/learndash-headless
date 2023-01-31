76afdba05661f76f9365757e6fe5ef63c971ee77
rohit155140 <rohitkumar63361@gmail.com>
2023-01-31 17:36:55 +0530

    update on model animation

--- components/card/MusicCard.js
+++ components/card/MusicCard.js
@@ -4,18 +4,21 @@ import Image from 'next/image'
 const MusicCard = () => {
 
     const [musicOn, setMusicOn] = useState(true);
-    const [musicOff, setMusicOff] = useState(false);
+
+
 
     return (
         <>
-            {/* {musicOff ? <div className='flex items-center justify-center space-x-2' onClick={() => setMusicOn(true)}>
-                <p className='font-semibold'>off</p>
-                <Image src="/images/sound.png" width={25} height={25} alt="sound" />
-            </div> : ""} */}
 
-            {musicOn ? <div className='flex items-center justify-center space-x-2' onClick={() => setMusicOff(true)}>
+
+            {musicOn ? <div className='flex items-center justify-center space-x-2' onClick={() => setMusicOn(false)}>
                 <p className='font-semibold'>on</p>
                 <Image src="/images/sound.gif" width={25} height={25} alt="sound" />
+            </div> : ""}
+
+            {!musicOn ? <div className='flex items-center justify-center space-x-2' onClick={() => setMusicOn(true)}>
+                <p className='font-semibold'>off</p>
+                <Image src="/images/sound.png" width={25} height={25} alt="sound" />
             </div> : ""}
 
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,18 +11,20 @@ import Fade from 'react-reveal/Fade';
 import MusicCard from 'components/card/MusicCard'
 
 const Test = () => {
-
-    const [clicked, setClicked] = useState(false);
-
     const [open, setOpen] = useState(false);
+    const [down, setDown] = useState(false);
     const HandleClick = () => {
-        setClicked(true)
+        setTimeout(() => { setOpen(true) }, 100)
     }
+
+    const changeDuration = () => {
+
+        setTimeout(() => setOpen(!open), 1000);
+    }
+
     return (
         <>
             {/* <WelcomeBackModel /> */}
-
-
 
             <Head> LearnDash </Head>
             <div className={`grid grid-cols-1 md:grid-cols-12   md:h-screen bg-[url('/images/bg-image.png')] bg-cover bg-center bg-no-repeat`}>
@@ -36,7 +38,7 @@ const Test = () => {
                 </div>
 
 
-                {!open ? <div className='col-span-5 grid grid-cols-12'>
+                {!open ? <div className='col-span-5 grid grid-cols-12 animateMain '>
                     <div className='col-span-6 md:h-screen md:col-span-6'>
                         <div className='grid md:h-full grid-cols-1 divide-y-[1px] divide-bordergray md:border-0 border-y-[1px] border-bordergray'>
                             <div className='flex flex-col justify-center border-l border-l-bordergray  p-2 zoom-effect-container'>
@@ -49,7 +51,7 @@ const Test = () => {
                                 </div>
                             </div>
 
-                            <div className='flex flex-col justify-center p-2 zoom-effect-container border-l border-l-bordergray' onClick={() => setOpen(true)}>
+                            <div className='flex flex-col justify-center p-2 zoom-effect-container border-l border-l-bordergray' onClick={() => HandleClick()}>
 
                                 <div className='flex space-x-4 md:block md:space-x-0 image-card '>
                                     <Image alt='start' src='/images/Im-Back.png' height='65' width='65' className='md:mx-auto ' />
@@ -58,7 +60,10 @@ const Test = () => {
                                         <p className='mt-2 hide md:text-center lineUp'>have a question? get in touch!</p>
                                     </div>
                                 </div>
+
                             </div>
+
+
 
 
                         </div>
@@ -98,72 +103,71 @@ const Test = () => {
 
                         </div>
                     </div>
-                </div> : <div className=' col-span-5 relative '>
+                </div> : ""}
 
-                    <div className={`animate top-1 left-1`}>
-
-                        <div className="   mt-48 px-40">
-                            <div className="w-full  m-auto bg-transparent  border border-bordergray p-4 lg:max-w-sm">
-                                <div className='flex justify-between items-center'>
-                                    <h2 className='font-semibold py-2'>Welcome Back !</h2>
-                                    <button onClick={() => setOpen(false)}><AiOutlineClose /></button>
-                                </div>
-
-                                <form className="">
-                                    <div className="mb-2">
-                                        <label
-                                            for="email"
-                                            className="block text-sm font-semibold text-gray-800"
-                                        >
-                                            Email
-                                        </label>
-                                        <input
-                                            type="email"
-                                            className="block w-full px-4 py-2 mt-2  bg-white border border-bordergray   focus:outline-none "
-                                            placeholder='enter your mail'
-                                        />
-                                    </div>
-                                    <div className="mb-2">
-                                        <label
-                                            for="password"
-                                            className="block text-sm font-semibold text-gray-800"
-
-                                        >
-                                            Password
-                                        </label>
-                                        <input
-                                            type="password"
-                                            className="block w-full px-4 py-2 mt-2  bg-white border  border-bordergray    focus:outline-none "
-                                            placeholder='**********'
-                                        />
-                                    </div>
-                                    <div className='flex justify-between items-center'>
-                                        <div className='flex space-x-1 items-center'>
-
-                                            <div class="form-check">
-                                                <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault" />
-
-                                            </div>
-
-                                            <p>remember me</p>
-                                        </div>
-                                        <Link href="#">
-                                            <p className='font-bold'>forget Password</p>
-                                        </Link>
-
-                                    </div>
-                                    <div className="mt-6 mx-6">
-                                        <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-black rounded-3xl focus:outline-none">
-                                            Login
-                                        </button>
-                                    </div>
-                                </form>
+                {open ? <div className={` col-span-5 relative  animateUp ${down == true ? 'animateDown' : ""} `} >
+                    <div className=" mt-10  px-40">
+                        <div className="w-full  m-auto bg-transparent  border border-bordergray p-4 lg:max-w-sm">
+                            <div className='flex justify-between items-center'>
+                                <h2 className='font-semibold py-2'>Welcome Back !</h2>
+                                <button onClick={() => { setDown(true), changeDuration() }}><AiOutlineClose /></button>
                             </div>
 
-                        </div>
-                    </div>
+                            <form className="">
+                                <div className="mb-2">
+                                    <label
+                                        for="email"
+                                        className="block text-sm font-semibold text-gray-800"
+                                    >
+                                        Email
+                                    </label>
+                                    <input
+                                        type="email"
+                                        className="block w-full px-4 py-2 mt-2  bg-white border border-bordergray   focus:outline-none "
+                                        placeholder='enter your mail'
+                                    />
+                                </div>
+                                <div className="mb-2">
+                                    <label
+                                        for="password"
+                                        className="block text-sm font-semibold text-gray-800"
 
-                </div>}
+                                    >
+                                        Password
+                                    </label>
+                                    <input
+                                        type="password"
+                                        className="block w-full px-4 py-2 mt-2  bg-white border  border-bordergray    focus:outline-none "
+                                        placeholder='**********'
+                                    />
+                                </div>
+                                <div className='flex justify-between items-center'>
+                                    <div className='flex space-x-1 items-center'>
+
+                                        <div class="form-check">
+                                            <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" value="" id="flexCheckDefault" />
+
+                                        </div>
+
+                                        <p>remember me</p>
+                                    </div>
+                                    <Link href="#">
+                                        <p className='font-bold'>forget Password</p>
+                                    </Link>
+
+                                </div>
+                                <div className="mt-6 mx-6">
+                                    <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-black hover:bg-voilet rounded-3xl focus:outline-none">
+                                        Login
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+
+                    : ""}
 
 
 
