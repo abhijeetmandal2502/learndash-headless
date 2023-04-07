02d97a53a68b2155c3ae9a2d7ef6030f77d20aae
Suraj Haldar <sh912548@gmail.com>
2023-04-07 14:40:43 +0530

    working on test page mobile drawer

--- src/pages/test.js
+++ src/pages/test.js
@@ -4,20 +4,66 @@ const Test = () => {
     return (
         <>
 
-            <div className='grid grid-cols-12 '>
 
-                <div className='col-span-12 md:col-span-6  h-full w-full' style={{ "height": "100vh" }}>
+            {/* <div class="drawer drawer-end">
+                <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+             
+                <div class="drawer-side">
+                    
+                    <label for="my-drawer  my-drawer-4" class="drawer-overlay"></label>
+                    <ul class="menu p-4 w-80 bg-base-100 text-base-content">
 
-                    <div className='w-full h-1/2 border border-black'>
-                        div1
-                    </div>
-                    <div className='w-full h-1/4 border border-black'>
-                        div2
-                    </div>
-                    <div className='w-full h-1/4 border border-black'>
-                        div3
-                    </div>
+                        <li><a>Sidebar Item 1</a></li>
+                        <li><a>Sidebar Item 2</a></li>
 
+                    </ul>
+                </div>
+                <div class="drawer-side">
+                    <label for="my-drawer-4" class="drawer-overlay"></label>
+                    <div class="drawer">
+                        <div>
+                            <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+                            <div class="drawer-content">
+
+                                <label for="my-drawer" class="btn btn-primary drawer-button">Open left</label>
+                            </div>
+                            <div class="drawer-side">
+                                <label for="my-drawer  my-drawer-4" class="drawer-overlay"></label>
+                                <ul class="menu p-4 w-80 bg-base-100 text-base-content">
+
+                                    <li><a>Sidebar Item 1</a></li>
+                                    <li><a>Sidebar Item 2</a></li>
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> */}
+            <div class="drawer drawer-end">
+                <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+                <div className="bg-black drawer-content">
+
+                    <label for="my-drawer-4" class="drawer-button btn btn-primary">Open drawer</label>
+                    <ul class="menu p-4 w-80  text-base-content">
+
+                        <li><a>Sidebar first</a></li>
+                        <li><a>Sidebar first</a></li>
+                    </ul>
+
+                </div>
+
+                <div class="drawer-side duration-500">
+                    <label for="my-drawer-4" class="drawer-overlay"></label>
+                    <div className='w-full h-full bg-white'>
+                        <label for="my-drawer-4" class="drawer-button btn btn-primary float-right">Open drawer</label>
+
+                        <ul class="menu p-4 w-80  text-base-content">
+
+                            <li><a>Sidebar second 1</a></li>
+                            <li><a>Sidebar second 2</a></li>
+                        </ul>
+                    </div>
                 </div>
 
             </div>
