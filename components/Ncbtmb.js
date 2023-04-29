6b4824bea6c81e92f8fda20fb0910cd03706b40e
Suraj Haldar <sh912548@gmail.com>
2023-04-29 17:11:19 +0530

    ncbt page code updated and index page images changes for mobile

--- components/Ncbtmb.js
+++ components/Ncbtmb.js
@@ -5,6 +5,7 @@ import { BiArrowBack, BiCheck } from 'react-icons/bi'
 import styles from '../src/styles/Ncbtmb.module.css'
 import Select from 'react-select';
 import { AiOutlineCheck } from 'react-icons/ai'
+import Link from 'next/link'
 
 
 const Ncbtmb = ({ ncbtmbMethodHide }) => {
@@ -78,26 +79,29 @@ const Ncbtmb = ({ ncbtmbMethodHide }) => {
 
   return (
     <>
-      <div className=' bg-transparent h-screen overflow-scroll '>
-
+      <div className='relative h-screen mx-4 overflow-hidden bg-transparent '>
+        {/* this loby button for desktop */}
         <button className={`md:flex hidden items-center space-x-1 bg-black text-white font-bold  ${styles.lobby} px-4 3xl:px-6 py-2 3xl:py-2.5 rounded-3xl `} type='button' onClick={() => ncbtmbMethodHide()}  >
-          <BiArrowBack size={20} className="text-white 3xl:w-8 3xl:h-8" /><span className='text-sm 3xl:text-2xl '>lobby</span></button>
-
-        <div className={`flex  2xl:space-x-20 max-lg:space-x-20 md:space-x-20  justify-between items-center  ${styles.ncbtapproved}`}>
+          <BiArrowBack size={20} className="text-white 3xl:w-8 3xl:h-8" /><span className='smallf '>lobby</span>
+        </button>
+        {/* loby button code end */}
+        <div className={`flex   mt-10 mb-6  md:my-0  2xl:space-x-20 max-lg:space-x-20 md:space-x-20  justify-between items-center  ${styles.ncbtapproved}`}>
           <div>
-            <h2 className=' tracking-wide font-normal   lg:text-[30px] xl:text-[40px] text-[23px]  2xl:text-[48px] md:text-[20px] md:leading-tight leading-normal '>all courses<br></br>ncbtmb<br></br>approved.</h2>
+            <h2 className='font-normal leading-tight tracking-wide fourxllargef'>all courses<br></br>ncbtmb<br></br>approved.</h2>
           </div>
 
-          <Image src="/images/ncbtmb.svg" height="323" width="323" alt="ncbtmb" className={``} />
+          <Image src="/images/ncbtmb.svg" height={323} width={323} alt="ncbtmb" className={`hidden md:block ncbt-desk-img`} />
+          <Image src="/images/ncbtmb.svg" height={200} width={200} alt="ncbtmb" className={`md:hidden absolute top-6 -right-4`} />
         </div>
         <form >
           <div className={`${styles.selectBox}`}>
 
-            <div className='py-2 3xl:text-4xl'>
-              accepted in your state?
+            <div className='py-2 font-semibold '>
+              <p className='hidden md:block mediumf'>accepted in your state?</p>
+              <p className=' md:hidden mediumf'>Select a State</p>
             </div>
             <Select
-              className={`2xl:w-96 w-full  z-[100] ${styles.selectBox} `}
+              className={`2xl:w-96 w-full smallf z-[100] ${styles.selectBox} `}
               defaultValue={selectedOption}
               onChange={setSelectedOption}
               options={options}
@@ -118,29 +122,37 @@ const Ncbtmb = ({ ncbtmbMethodHide }) => {
           </div>
 
         </form>
-        <div className={`pt-3 ${styles.ncbtapproved} ${selectedOption ? styles.showCourseDetail : styles.hideCourseDetail}`}>
+        <div className={`pt-6 md:pt-3 ${styles.ncbtapproved} ${selectedOption ? styles.showCourseDetail : styles.hideCourseDetail}`}>
 
-          <div className='flex  items-center space-x-2'>
+          <div className='flex items-start my-4 space-x-2 md:my-0 md:items-center'>
             <div className='md:p-2 p-0.5 rounded-full bg-[#2AC368]'>
               <BiCheck size={25} className='text-white md:text-[25px] text-[16px]' />
             </div>
-            <h3 className='md:text-[20px] text-[14px]'>Texas? yep! you’re fine.</h3>
-            <button className=' border-b border-voilet text-voilet leading-6 md:text-3xl text-md'> click here.</button>
+            <h3 className='leading-none triplelargef'>Texas? yep! you’re fine.<br /><Link href="/"></Link><div className='border-b w-max md:hidden border-voilet text-voilet' >click here.</div></h3>
+            <button className='hidden leading-6 border-b md:block border-voilet text-voilet md:text-3xl text-md'> click here.</button>
           </div>
 
           <div className='py-3'>
             {courseData && courseData.map((item, index) => {
               return (
-                <div key={index} className='flex items-center space-x-4 py-0.3'>
-                  <AiOutlineCheck size={20} className='text-[#9747FF]' />
-                  <p className=' text-[12px] max-lg:text-[15px]'>{item}</p>
+                <div key={index}>
+                  <div className='md:flex hidden  items-center space-x-4 py-0.3'>
+                    <AiOutlineCheck size={20} className='text-[#9747FF]' />
+                    <p className=' smallf max-lg:text-[15px]'>{item}</p>
+                  </div>
+                  <div className='grid grid-cols-12 md:hidden '>
+                    <div className='col-span-1'>
+                      <AiOutlineCheck size={20} className='mt-1 text-[#9747FF]' />
+                    </div>
+                    <div className='col-span-11'>
+                      <p className='mediumf'>{item}</p>
+                    </div>
+                  </div>
                 </div>
               )
             })}
           </div>
-
         </div>
-
       </div>
 
     </>
