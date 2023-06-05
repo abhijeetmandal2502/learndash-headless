f86c9348989a3b58f7ab06b16a53d156f30f77bd
Suraj Haldar <sh912548@gmail.com>
2023-06-05 17:29:58 +0530

    env file change

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
 
-  env: {
-    API_BASE_URL: 'https://cesimple.wpengine.com/wp-json'
-
-  },
   images: {
     domains: ['h2d0qdmatmf2l6agp7tn7y36y.js.wpenginepowered.com', 'h5dy7uj4qf3om2qd3ma359c2g.js.wpenginepowered.com']
   },
