b83cfb2ff9a8b040cb9ab980ff2559136c119d9e
Rohit Kumar <54665399+ROHIT155140@users.noreply.github.com>
2023-06-19 19:10:35 +0530

    add logout btn

--- components/card/TotalCartItems.js
+++ components/card/TotalCartItems.js
@@ -6,6 +6,7 @@ import styles from '../../src/styles/NewIndex.module.css'
 import { productCartState } from 'recoil/atoms';
 import { getCartItems } from 'utils/addToCart';
 import { useRecoilState } from 'recoil'
+import Link from 'next/link';
 const TotalCartItems = () => {
 
   const [productsRecoil, setProductsRecoil] = useRecoilState(productCartState)
@@ -18,21 +19,23 @@ const TotalCartItems = () => {
   }, [])
 
   return (
-    <div
-      className=" container"
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-    >
-      <div className={`transition-all ease-in duration-1000 w-max ${styles.cartAnimation}`}>
-        <Image
-          src={isHovering ? arrowHover : arrow} width={120} height={120}
-          className={` ${isHovering ? 'transition-all ease duration-2000' : "transition-all ease- duration-2000"}  cartImage`}
-        />
+    <Link href="/courses">
+      <div
+        className=" container"
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+      >
+        <div className={`transition-all ease-in duration-1000 w-max ${styles.cartAnimation}`}>
+          <Image
+            src={isHovering ? arrowHover : arrow} width={120} height={120}
+            className={` ${isHovering ? 'transition-all ease duration-2000' : "transition-all ease- duration-2000"}  cartImage`}
+          />
+        </div>
+        <div className='absolute  right-4 w-5 h-5 text-white -translate-x-0 -translate-y-0 rounded-full bg-voilet top-2 z-50' onClick={() => alert('mouse enter')}>
+          <p className='flex items-center justify-center text-[12px]'>{productsRecoil.length}</p>
+        </div>
       </div>
-      <div className='absolute  right-4 w-5 h-5 text-white -translate-x-0 -translate-y-0 rounded-full bg-voilet top-2 z-50' onClick={() => alert('mouse enter')}>
-        <p className='flex items-center justify-center text-[12px]'>{productsRecoil.length}</p>
-      </div>
-    </div>
+    </Link>
   )
 }
 
