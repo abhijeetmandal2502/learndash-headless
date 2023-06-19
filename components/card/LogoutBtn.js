cca4d9f9e5c8b2efde3a5f039025c7e684835f50
Rohit Kumar <54665399+ROHIT155140@users.noreply.github.com>
2023-06-19 20:07:00 +0530

    update login model script

--- components/card/LogoutBtn.js
+++ components/card/LogoutBtn.js
@@ -2,14 +2,26 @@ import Image from 'next/image'
 import React from 'react'
 import styles from '../../components/IwannaTech/IwanntTech.module.css'
 import Cookies from 'js-cookie'
+import { useRouter } from 'next/router'
 
 
 const LogoutBtn = () => {
+
+    const router = useRouter();
     const token = Cookies.get('token');
+
+    const handleLogout = () => {
+
+        Cookies.remove('token', { path: '/' }) // removed!
+        setTimeout(() => {
+            router.reload("/")
+        }, 100)
+    }
+
 
     return (
         <>
-            {token && <div className={`relative ${styles.logoutBtnShadow}`}>
+            {token && <div className={`relative ${styles.logoutBtnShadow}`} onClick={() => handleLogout()} >
                 <div>
                     <Image src="/images/Logout.svg" width={70} height={70} className='' alt="logout" />
                 </div>
